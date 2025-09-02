1d39042f85bb0e7c1306fbe6fd6ebf6c2bbc454c
Samuelms <samuel.sarimo@gmail.com>
2025-09-02 10:38:12 +0300

    Setup JavaFX, add JPA,hibernate,mariaDB jdbc and Junit dependencies, add project-plan and sprint 1 report

---
+++ src/main/java/application/view/GUI.java
@@ -0,0 +1,14 @@
+package application.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Shout!");
+        stage.setWidth(600);
+        stage.setHeight(600);
+        stage.show();
+    }
+}
