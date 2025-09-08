9351546c61342abe499d61911ed5137acb9acc3a
aruraruri <aurilait@gmail.com>
2025-09-08 08:46:36 +0300

    Remove unneeded imports

--- src/test/java/application/controller/ControllerTest.java
+++ src/test/java/application/controller/ControllerTest.java
@@ -1,23 +1,6 @@
 package application.controller;
 
-import application.view.GUI;
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import javafx.event.ActionEvent;
-
-
-import java.io.IOException;
-import java.net.URL;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
@@ -32,7 +15,7 @@ class ControllerTest {
     }
 
     @Test
-    void register() throws IOException {
+    void register() {
     }
 
     @Test
