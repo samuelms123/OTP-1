84deef42efe7426eafe0cfc806b4eb6e484296a8
aruraruri <aurilait@gmail.com>
2025-09-08 08:38:40 +0300

    Add jacoco and some test scaffolding

---
+++ src/test/java/application/controller/ControllerTest.java
@@ -0,0 +1,49 @@
+package application.controller;
+
+import application.view.GUI;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import javafx.event.ActionEvent;
+
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void login() {
+
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void register() throws IOException {
+    }
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void openProfilePage() {
+    }
+
+    @Test
+    void openFeedPage() {
+    }
+}
---
+++ src/test/java/application/view/GUITest.java
@@ -0,0 +1,20 @@
+package application.view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GUITest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void getStage() {
+    }
+}
