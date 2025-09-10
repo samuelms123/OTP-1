c97f3fd911048fe941392653e307d2bf71b33f99
Samuelms <samuel.sarimo@gmail.com>
2025-09-10 16:04:56 +0300

    Write unit tests for register and login, add mockito dependency for mocking database conn.

---
+++ src/test/java/application/model/service/UserServiceTest.java
@@ -0,0 +1,80 @@
+package application.model.service;
+
+import application.model.data_objects.LoginResult;
+import application.model.data_objects.RegistrationResult;
+import application.model.entity.User;
+import dao.UserDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class UserServiceTest {
+    @Mock
+    private UserDao userDao;
+    @InjectMocks
+    private UserService userService;
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this); // init mocks
+        user = new User();
+        user.setEmail("test@example.com");
+        user.setUsername("testuser");
+
+    }
+    @Test
+    void registerShouldFailWhenEmailIsTaken() {
+        when(userDao.isUserEmailUnique(user.getEmail())).thenReturn(false); // mock behaviour
+
+        RegistrationResult result = userService.registerUser(user);
+        assertFalse(result.isSuccess()); // assert registration failure
+        assertEquals("Email already taken", result.getMessage());
+        verify(userDao, never()).persist(any(User.class)); // Basically verifies that persist method is not called during this test, because the email is already taken.
+    }
+
+    @Test
+    void registerShouldFailWhenUsernameIsTaken() {
+        when(userDao.isUserEmailUnique(user.getEmail())).thenReturn(true); // Need to mock the email to pass because of registerUser() execute order
+        when(userDao.isUserNameUnique(user.getUsername())).thenReturn(false);
+
+        RegistrationResult result = userService.registerUser(user);
+        assertFalse(result.isSuccess());
+        assertEquals("Username already taken", result.getMessage());
+        verify(userDao, never()).persist(any(User.class));
+    }
+
+    @Test
+    void shouldRegisterSuccessfully() {
+        when(userDao.isUserEmailUnique(user.getEmail())).thenReturn(true);
+        when(userDao.isUserNameUnique(user.getUsername())).thenReturn(true);
+
+        RegistrationResult result = userService.registerUser(user);
+        assertTrue(result.isSuccess());
+        assertEquals("User registered successfully", result.getMessage());
+        verify(userDao).persist(user); // Verify that the persist method is called!
+    }
+
+    @Test
+    void loginShouldFail() {
+        when(userDao.findUser(user.getUsername(), user.getPassword())).thenReturn(null);
+
+        LoginResult result = userService.loginUser(user);
+        assertFalse(result.isSuccess());
+        assertEquals("Username not found or password incorrect", result.getMessage());
+    }
+
+    @Test
+    void loginShouldSuccess() {
+        when(userDao.findUser(user.getUsername(), user.getPassword())).thenReturn(user);
+        LoginResult result = userService.loginUser(user);
+        assertTrue(result.isSuccess());
+        assertEquals("Login successful!", result.getMessage());
+    }
+}
